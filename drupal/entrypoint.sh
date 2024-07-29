b09f87a42c2ec64d4bf25db2dab7cc17b26b09c6
Kai Amann <kai.amann@fau.de>
2024-07-29 17:55:45 +0200

    build: Use docker healthcheck
    Use the docker-compose healthcheck directive to check when mariadb is ready instead of doing it in the entrypoint.sh
    Remove the now unnecessary mysql dependency

--- drupal/entrypoint.sh
+++ drupal/entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 # Install the Drupal site with SCS Manager
 
-until mysql -h mariadb -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;" > /dev/null 2>&1; do
-  echo "Waiting for MariaDB to be ready..."
-  sleep 5
-done
 
 # Install the site
-drush si --db-url="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb:3306/${MYSQL_DATABASE}" --site-name="${SITE_NAME}" --account-name="${DRUPAL_USER}" --account-pass="${DRUPAL_PASSWORD}"
+drush si \
+  --db-url="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb:3306/${MYSQL_DATABASE}" \
+  --site-name="${SITE_NAME}" \
+  --account-name="${DRUPAL_USER}" \
+  --account-pass="${DRUPAL_PASSWORD}"
 
-# Install modules
+# Install development modules
 composer require drupal/devel kint-php/kint
+drush en devel -y
+
+# Install and enable scs module
 git clone https://github.com/SODa-Collections-Objects-Data-Literacy/scs-manager.git /var/www/html/modules/custom/soda_scs_manager
-
-
-# Enable the module
-drush en soda_scs_manager,devel -y
+drush en soda_scs_manager -y
 
 # Set permissions
 chown -R www-data:www-data /opt/drupal
