633c51a006c844214109f8550982a1c26f62e9f3
rnsrk <robert@nasarek.org>
2024-08-28 05:52:14 +0000

    impl deploy auth @ ghcr.io

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export $(cat .env) > /dev/null 2>&1; 
 
-docker stack deploy -c docker-compose.yml scs-manager
+docker stack deploy --with-registry-auth -c docker-compose.yml scs-manager
