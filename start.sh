06f937e7920d1bdd0124e241d9a2a5ca742a9d20
rnsrk <robert@nasarek.org>
2024-08-29 12:43:57 +0000

    now with ts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export $(cat .env) > /dev/null 2>&1; 
 
-docker stack deploy --with-registry-auth -c docker-compose.yml scs-manager
+docker stack deploy --detach=true --with-registry-auth -c docker-compose.yml scs-manager
