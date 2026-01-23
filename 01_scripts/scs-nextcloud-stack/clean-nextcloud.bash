7db5a6ac212c969cc232279b0b6f2d7c666afc77
rnsrk <robert@nasarek.dev>
2026-01-23 09:43:25 +0100

    extend nextcloud scripts

--- 01_scripts/scs-nextcloud-stack/clean-nextcloud.bash
+++ 01_scripts/scs-nextcloud-stack/clean-nextcloud.bash
@@ -1,31 +1,47 @@
 #!/bin/bash
 
-set -x
+set -e
 
-# Check if --skip-env parameter is provided (when called from parent script).
-SKIP_LOCAL_ENV=false
-if [[ "$1" == "--skip-env" ]] || [[ "$1" == "--no-local-env" ]]; then
-    SKIP_LOCAL_ENV=true
-fi
-
-# Load environment variables from local .env if not skipped.
-if [ "$SKIP_LOCAL_ENV" = false ] && [ -f .env ]; then
+# Load environment variables.
+if [ -f .env ]; then
     source .env
+    echo "Loaded environment variables from .env file."
+else
+    echo "No .env file found. Please create one and set the environment variables. Have you execute the script from the root directory?"
+    exit 1
 fi
 
-# Check if MARIADB_ROOT_PASSWORD is set.
+# Check if SCS_DB_ROOT_PASSWORD is set.
 if [ -z "${SCS_DB_ROOT_PASSWORD}" ]; then
     echo "Error: SCS_DB_ROOT_PASSWORD environment variable is not set."
     exit 1
 fi
 
-docker compose down nextcloud-nextcloud nextcloud-onlyoffice-document-server nextcloud-nextcloud-reverse-proxy
-# Wait for 15 seconds to ensure all containers are properly stopped.
-echo "Waiting for 15 seconds to ensure all containers are properly stopped..."
-sleep 10
-docker volume remove soda_scs_manager_deployment_nextcloud-data soda_scs_manager_deployment_onlyoffice-data soda_scs_manager_deployment_onlyoffice-log
-docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS nextcloud;"
-docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS onlyoffice;"
-docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP USER IF EXISTS 'nextcloud'@'%';"
-docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP USER IF EXISTS 'onlyoffice'@'%';"
-docker exec scs--databasee mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+# Check if NEXTCLOUD_DB_NAME is set.
+if [ -z "${NEXTCLOUD_DB_NAME}" ]; then
+    echo "Error: NEXTCLOUD_DB_NAME environment variable is not set."
+    exit 1
+fi
+
+# Check if NEXTCLOUD_DB_USER is set.
+# Clean up Nextcloud.
+read -p "Caution: This operation will permanently delete your database: ${NEXTCLOUD_DB_NAME} and user: ${NEXTCLOUD_DB_USER}. This is not reversible. Are you sure you want to proceed? (yes/no): " confirm
+if [[ "$confirm" != "yes" ]]; then
+    echo "Not typed 'yes'. Aborted by user."
+    exit 0
+fi
+
+
+echo "Cleaning up Nextcloud..."
+docker compose down -v nextcloud--nextcloud nextcloud--onlyoffice-document-server nextcloud--nextcloud-reverse-proxy nextcloud--onlyoffice-reverse-proxy nextcloud--redis
+
+# Drop Nextcloud database and user.
+echo "Dropping Nextcloud database and user..."
+docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS ${NEXTCLOUD_DB_NAME};"
+echo "Nextcloud database dropped successfully."
+docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "DROP USER IF EXISTS '${NEXTCLOUD_DB_USER}'@'%';"
+echo "Nextcloud user dropped successfully."
+docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+echo "Privileges flushed successfully."
+
+echo "Nextcloud cleaned up successfully."
--- 01_scripts/scs-nextcloud-stack/pre-install.bash
+++ 01_scripts/scs-nextcloud-stack/pre-install.bash
@@ -1,38 +1,58 @@
 #!/bin/bash
 
-set -x
+set -e
 
 # Load environment variables.
 if [ -f .env ]; then
     source .env
+    echo "Loaded environment variables from .env file."
+else
+    echo "No .env file found. Please create one and set the environment variables. Have you execute the script from the root directory?"
+    exit 1
 fi
 
-# Check if required environment variables are set.
+# Check if SCS_DB_ROOT_PASSWORD is set.
 if [ -z "${SCS_DB_ROOT_PASSWORD}" ]; then
     echo "Error: SCS_DB_ROOT_PASSWORD environment variable is not set."
     exit 1
 fi
 
+# Check if NEXTCLOUD_DB_PASSWORD is set.
 if [ -z "${NEXTCLOUD_DB_PASSWORD}" ]; then
     echo "Error: NEXTCLOUD_DB_PASSWORD environment variable is not set."
     exit 1
 fi
 
+# Check if NEXTCLOUD_DB_NAME is set.
 if [ -z "${NEXTCLOUD_DB_NAME}" ]; then
-    NEXTCLOUD_DB_NAME="nextcloud"
+    echo "NEXTCLOUD_DB_NAME environment variable is not set. Using default value: ${NEXTCLOUD_DB_NAME}"
 fi
 
+# Check if NEXTCLOUD_DB_USER is set.
 if [ -z "${NEXTCLOUD_DB_USER}" ]; then
-    NEXTCLOUD_DB_USER="nextcloud"
+    echo "NEXTCLOUD_DB_USER environment variable is not set. Using default value: ${NEXTCLOUD_DB_USER}"
 fi
 
+# Check if NEXTCLOUD_DB_PASSWORD is set.
 if [ -z "${NEXTCLOUD_DB_PASSWORD}" ]; then
     echo "Error: NEXTCLOUD_DB_PASSWORD environment variable is not set."
     exit 1
 fi
 
 # Create Nextcloud database.
+echo "Creating Nextcloud database..."
 docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${NEXTCLOUD_DB_NAME};"
+echo "Nextcloud database created successfully."
+
+echo "Creating Nextcloud user..."
 docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${NEXTCLOUD_DB_USER}'@'%' IDENTIFIED BY '${NEXTCLOUD_DB_PASSWORD}';"
+echo "Nextcloud user created successfully."
+
+echo "Granting privileges..."
 docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${NEXTCLOUD_DB_NAME}.* TO '${NEXTCLOUD_DB_USER}'@'%';"
+echo "Privileges granted successfully."
+
+echo "Flushing privileges..."
 docker exec scs--database mariadb -u root -p"${SCS_DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+echo "Nextcloud database and user created successfully."
